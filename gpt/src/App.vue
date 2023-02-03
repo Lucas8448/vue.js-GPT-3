451010083041b9f77aa6141232803fd51fe2b5b4
Lucas Bateson <lucas.bateson@gmail.com>
2023-02-03 13:29:04 +0100

    Stable Diffusion Test

--- gpt/src/App.vue
+++ gpt/src/App.vue
@@ -1,8 +1,14 @@
 <template>
+  <div class="title">
+    <h1>ChatBot<font size=1>Powered by OpenAI</font></h1>
+  </div>
   <div class="container">
-    <div class="chats">
-      <div class="chats-title">
-        <h1>Chats</h1>
+    <div class="options">
+      <div class="options-title">
+        <h1>Options</h1>
+      </div>
+      <div class="clear-chat">
+        <button @click="clearChat">Clear Chat</button>
       </div>
     </div>
     <div class="chat-container" ref="chatContainer">
@@ -32,6 +38,7 @@ export default {
       messages: [],
       selectedPreset: null,
       presets: [],
+      previousChats: null
     }
   },
   updated() {
@@ -89,12 +96,25 @@ export default {
           url: this.selectedPreset.url,
           body: { ...this.selectedPreset.body, prompt: "Reply in a rude manner using curse words: " + this.prompt }
         }
+      } else if (this.selectedPreset.name == "Stable Diffusion Prompt") {
+        return {
+          name: this.selectedPreset.name,
+          url: this.selectedPreset.url,
+          body: {
+            ...this.selectedPreset.body, prompt: "Create a new unique prompt with the same format as these ones: 'portrait photo of a asia old warrior chief, tribal panther make up, blue on red, side profile, looking away, serious eyes, 50mm portrait photography, hard rim lighting. or city made out of glass : : close shot : : 3 5 mm, realism, octane render, 8 k, exploration, cinematic, trending on artstation, realistic, 3 5 mm camera, unreal engine, hyper detailed, photo – realistic maximum detail, volumetric light, moody cinematic epic concept art, realistic matte painting, hyper photorealistic, concept art, volumetric light, cinematic epic, octane render, 8 k, corona render, movie concept art, octane render, 8 k, corona render, cinematic, trending on artstation, movie concept art, cinematic composition, ultra – detailed, realistic, hyper – realistic, volumetric lighting, 8 k. or A type specimen poster showing every letter in the alphabet where each letter is made of pieces of simple shapes like circles, squares, triangles, and diamonds like color-forms very colorful and vibrant::2 poster in the international typographic style showing line-art illustrations of a full color spectrum typeface graphic design —h 432 —vibe. Using this initial promt (no repeating keywords): " + this.prompt }
+        }
       } else {
         console.log("no matching preset")
       }
     }
   },
   mounted() {
+    window.addEventListener('beforeunload', (event) => {
+      localStorage.setItem('previousChats', JSON.stringify(this.messages))
+    });
+    this.previousChats = JSON.parse(localStorage.getItem('previousChats')) || []
+    console.log("Loaded chats: " + this.previousChats)
+    this.messages = [...this.previousChats]
     this.presets = [
       {
         name: "Chat",
@@ -188,6 +208,20 @@ export default {
           "presence_penalty": 0.6,
           "stop": [" Human:", " AI:"]
         }
+      },
+      {
+        name: "Stable Diffusion Prompt",
+        url: 'https://api.openai.com/v1/completions',
+        body: {
+          "model": "text-davinci-003",
+          "prompt": "Create a Positive and Negative prompt based on three key words (medium, motive, description/details), where the positive prompt uses 1/6 as meduim, 2/6 as motive and 2-3/6 as description/details" +  this.prompt,
+          "temperature": 0,
+          "max_tokens": 150,
+          "top_p": 1,
+          "frequency_penalty": 0,
+          "presence_penalty": 0.6,
+          "stop": [" Human:", " AI:"]
+        }
       }
     ]
     this.selectedPreset = this.presets[0]
@@ -208,16 +242,21 @@ export default {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
-            'Authorization': `Bearer YOUR_API_KEY`
+            'Authorization': `Bearer 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`
           },
           body: JSON.stringify(this.currentPreset.body)
         });
         this.prompt = "";
         const json = await response.json();
+        console.log(json);
         this.messages.push({ id: Date.now(), sender: "AI", text: json.choices[0].text });
       } catch (error) {
         console.error(error);
       }
+    },
+    async clearChat() {
+      this.messages = []
+      localStorage.setItem('previousChats', JSON.stringify(this.messages))
     }
   }
 }
